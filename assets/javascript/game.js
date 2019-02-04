0104be4d3b5db01294716f1adc2d635f8bc23496
Jonelle Carless <jonellecarless@Jonelles-MBP.fios-router.home>
2019-02-04 14:44:30 -0500

    corrected html and js files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-$( document ).ready(function() {
-
 // Crystal Variables
 var crystal = {
     blue:
@@ -23,190 +21,86 @@ var crystal = {
       value: 0
     }
   };
-  
-  // Scores (Current and Target)
-  var currentScore = 0;
-  var targetScore = 0;
-  
-  // Wins and Losses
-  var winCount = 0;
-  var lossCount = 0;
+
+var currentScore = 0;
+var luckyNumber = 0;
+var winCount = 0;
+var lossCount = 0;
 
 
 // FUNCTIONS
+
+// function for random numbers
 var getRandom = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// start/restart game
 var startGame = function() {
   currentScore = 0;
-  targetScore = getRandom (1, 50);
+  luckyNumber = getRandom (10, 50);
 
   crystal.blue.value = getRandom(1, 15);
   crystal.yellow.value = getRandom(1, 15);
   crystal.green.value = getRandom(1, 15);
   crystal.red.value = getRandom(1, 15);
 
-  $("#score").html(currentScore);
-  $("#lucky").html(targetScore);
+  $("#score").text(currentScore);
+  $("#lucky").text(luckyNumber);
 
     // Testing Console
     console.log("-----------------------------------");
-    console.log("Target Score: " + targetScore);
-    console.log("Blue: " + crystal.blue.value + " | Green: " + crystal.green.value + " | Red: " + crystal.red.value +
-      " | Yellow: " + crystal.yellow.value);
+    console.log("Lucky Number: " + luckyNumber);
+    console.log("Blue: " + crystal.blue.value + " | Yellow: " + crystal.yellow.value + " | Greem: " + crystal.green.value +
+      " | Red: " + crystal.red.value);
     console.log("-----------------------------------");
 
 };
 
+// checking wins/losses
+var checkWin = function()
+ {
+   if (currentScore > luckyNumber) {
+     alert("Sorry, you lost.")
+     console.log("You lost");
+     lossCount++;
+     $("#losses").text(lossCount);
+     startGame();
+   }
 
+   else if (currentScore === luckyNumber) {
+     alert("You won!");
+     winCount++;
+     $("#wins").text(winCount);
+     startGame();
+   }
+ };
+
+
+ // adding values to the crystals
+var addValues = function(clickedCrystal) {
+  currentScore += clickedCrystal.value;
+  $("#score").text(currentScore);
+  checkWin();
+  console.log("Your score: " + currentScore);
+};
+
+
+// main function
+startGame();
+
+$("#blue").click(function()
+{addValues(crystal.blue);
 });
 
+$("#yellow").click(function() {
+  addValues(crystal.yellow);
+});
 
+$("#green").click(function() {
+  addValues(crystal.green);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Create global variables
-    var firstNumber = [];
-    var luckyNumber = [];
-    var userAnswer;
-    var isChosen = false;
-    var isCalculated = false;
-
-    
-    // var secondNumber = "";
-    // var thirdNumber = "";
-    // var fourthNumber = "";
-
-
-  //  function newGame ( {
-        firstNumber = [],
-        luckyNumber = [],
-        userAnswer,
-        isChosen = false,
-        isCalculated = false,
-    
-        
-
-
-        // Random Number is generated for Lucky Number Function
-        $("#lucky-number").on("click", function() {
-
-        for (var i = 0; i < 1; i++) {
-
-            var luckyNumber = Math.floor(Math.random() * 9) + 1;   
-
-         console.log(luckyNumber)
-
-        $("#lucky-number").html("<br><hr>" + luckyNumber); 
-        };
-
-
-        // Random number is generated for Red Button
-        $("#red").on("click", function () {
-            for (var i = 0; i < 1; i++) {
-                var firstNumber = Math.floor(Math.random() * 9) + 1;
-         
-            console.log(firstNumber)
-
-            // document.getElementById("red").innerHTML = firstNumber;
-
-        }
-    
-        //... and then dump the random number into our random-number div.
-            $("#red").html("<h1>" + firstNumber);
-
-        
-    // Get id of element clicked by user
-    $("#red").click(function(event) {
-    userAnswer = event.target.id;
-
-    // Compare id to target id
-    if (userAnswer === luckyNumber) {
-      //add a point to wins
-        alert("You win!");
-      reset();
-    }
-    else if (userAnswer < luckyNumber) {
-    //add difference in points to total score
-      alert("Try again!");
-      //reset ();
-    }
-    else (userAnswer > luckyNumber)
-    alert("You lose!")
-    // a point is added to losses
-    //reset ();
-  });
-
-        // $(".btn").on("click", function() {
-        // if (isCalculated) {
-        //     return false;
-        //   }
-        //   else {
-        //     firstNumber += $(this).val();
-        //     $("#red-button").text(firstNumber);
-        //   }
-
-
-
-
-
-        
-        
-        
-        
-
-    
-
-
-
-    
-    
-    
-
-
-
-
-
-    // Clear the divs and arrays upon each round.
-    // function clear() {
-    //     $("#random-number").empty();
-    //     $("red-button").empty();
-    //     luckyNumber = [];
-    //     redButton = [];
-    // }
-
-    // Function for resetting numbers and setting new numbers.
-    // function reset() {
-    //     clear();
-    //     redButton();
-    //     luckyNumber();
-    // }
-
-        //     // Clears previously selected values
-        // $("#random-number #red-button").empty();
-        // }
-
-
-
- // $("#red").toggle("click");
-    // Run game
-    //  reset();
-
-
-        });
-    });
+$("#red").click(function() {
+  addValues(crystal.red);
+});
