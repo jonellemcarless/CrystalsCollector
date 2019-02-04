e7c2772dfbd4783420f4aa2d71063546a94f19b7
Jonelle Carless <jonellecarless@Jonelles-MBP.fios-router.home>
2019-02-04 11:08:34 -0500

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,83 @@
-$(document).ready(function() {
+$( document ).ready(function() {
 
-// Game Logic for Crystal Collector
+// Crystal Variables
+var crystal = {
+    blue:
+    {
+      name: "Blue",
+      value: 0
+    },
+    yellow:
+    {
+      name: "Yellow",
+      value: 0
+    },
+    green:
+    {
+      name: "Green",
+      value: 0
+    },
+    red:
+    {
+      name: "Red",
+      value: 0
+    }
+  };
+  
+  // Scores (Current and Target)
+  var currentScore = 0;
+  var targetScore = 0;
+  
+  // Wins and Losses
+  var winCount = 0;
+  var lossCount = 0;
+
+
+// FUNCTIONS
+var getRandom = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var startGame = function() {
+  currentScore = 0;
+  targetScore = getRandom (1, 50);
+
+  crystal.blue.value = getRandom(1, 15);
+  crystal.yellow.value = getRandom(1, 15);
+  crystal.green.value = getRandom(1, 15);
+  crystal.red.value = getRandom(1, 15);
+
+  $("#score").html(currentScore);
+  $("#lucky").html(targetScore);
+
+    // Testing Console
+    console.log("-----------------------------------");
+    console.log("Target Score: " + targetScore);
+    console.log("Blue: " + crystal.blue.value + " | Green: " + crystal.green.value + " | Red: " + crystal.red.value +
+      " | Yellow: " + crystal.yellow.value);
+    console.log("-----------------------------------");
+
+};
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Create global variables
@@ -133,5 +210,3 @@ $(document).ready(function() {
 
         });
     });
-
-});
